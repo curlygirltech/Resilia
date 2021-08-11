bc837460bb77d8702b952710192d71cae918365e
jcameron10 <jovonnec@gmail.com>
2021-08-11 00:19:38 -0400

    set up FE & BE

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { useEffect } from react
+import axios from axios 
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <img src= "https://images.squarespace-cdn.com/content/v1/5e45fd7b05ae4e3f2f2ed60f/1581645908407-OU71JDZN3L0QV38JKDGQ/Resilia_Final_FullColor.jpg?format=1500w"/>
+    </div>
+  );
+}
+
+export default App;
