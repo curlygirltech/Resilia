930efe9e1644afa646a7c13caaf0150e65cd4d88
jcameron10 <jovonnec@gmail.com>
2021-08-11 11:34:23 -0400

    adding API to FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import { useEffect } from react
-import axios from axios 
+import { useEffect,useState } from "react"
+import axios from "axios"
 import './App.css';
+import Notification from './Components/Notification';
 
 function App() {
+  const [notification, setNotification] = useState([])
+
+  useEffect(() => {
+    const getNotifications = async () => {
+      const resp = await axios.get("http://localhost:3000/notifications");
+      setNotification(resp.data);
+    };
+    getNotifications();
+  })
 
   return (
     <div className="App">
-      <img src= "https://images.squarespace-cdn.com/content/v1/5e45fd7b05ae4e3f2f2ed60f/1581645908407-OU71JDZN3L0QV38JKDGQ/Resilia_Final_FullColor.jpg?format=1500w"/>
+      <img src="https://images.squarespace-cdn.com/content/v1/5e45fd7b05ae4e3f2f2ed60f/1581645908407-OU71JDZN3L0QV38JKDGQ/Resilia_Final_FullColor.jpg?format=1500w" />
+      <Notification notification={notification}/>
     </div>
   );
 }
