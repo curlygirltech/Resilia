00fdf3ead6cc6ed26b12463fa0483b5e87bc0553
jcameron10 <jovonnec@gmail.com>
2021-08-12 14:46:07 -0400

    debug api-helper

--- client/src/Services/apiHelper.js
+++ client/src/Services/apiHelper.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://resilia-take-home.herokuapp.com' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://resilia-take-home.netlify.app.com' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
